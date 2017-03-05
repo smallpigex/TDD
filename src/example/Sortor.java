86e7982b2928365544890aa51acb8de47260e94a
smallpigex <smallpigex@gmail.com>
2017-03-05 13:27:46 +0800

    opt ok

--- src/example/Sortor.java
+++ src/example/Sortor.java
@@ -1,17 +1,41 @@
 package example;
 
 public class Sortor {
-  public int[] sortValue(int[] values) {
-	  //Bubble Sort
-	  for(int i = 0; i < values.length; i++) {
-		  for(int j = 0; j < values.length; j++) {
-			  if(values[i] > values[j]) {
-				  int tmp = values[i];
-				  values[i] = values[j];
-				  values[j] = tmp;
-			  }
-		  }
-	  }
-	  return values;
-  }
+	private int[] arr;
+
+	public int[] sortValue(int[] values) {
+		// quick Sort
+		arr = values;
+		quickSort(0, arr.length - 1);
+		return arr;
+	}
+
+	private void quickSort(int start, int end) {
+		if (start >= end) {
+			return;
+		}
+		int pivote = arr[end];
+		int left = start, right = end - 1;
+		while (left < right) {
+			while (arr[left] <= pivote && left < right)
+				left++;
+			while (arr[right] >= pivote && left < right)
+				right--;
+			swap(left, right);
+		}
+		if (arr[left] >= arr[end]) {
+			swap(left, end);
+		} else {
+			left++;
+		}
+
+		quickSort(start, left - 1);
+		quickSort(left + 1, end);
+	}
+
+	private void swap(int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
 }
