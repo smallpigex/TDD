0ac5a74ed152290f128af1bfde446cb6357ef88e
smallpigex <smallpigex@gmail.com>
2017-03-05 11:40:32 +0800

    通過測試

--- src/example/Sortor.java
+++ src/example/Sortor.java
@@ -2,6 +2,12 @@ package example;
 
 public class Sortor {
   public int[] sortValue(int[] values) {
+	  
+	  if(values.length > 1 && values[0] > values[1]) {
+		  int tmp = values[0];
+		  values[0] = values[1];
+		  values[1] = tmp;
+	  }
 	  return values;
   }
 }
