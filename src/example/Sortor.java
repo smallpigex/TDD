20990f206730b9329b190af18bc0a375c1845b4a
smallpigex <smallpigex@gmail.com>
2017-03-11 20:12:48 +0800

    Update Sortor.java

--- src/example/Sortor.java
+++ src/example/Sortor.java
@@ -4,7 +4,7 @@ public class Sortor {
   public int[] sortValue(int[] values) {
 	  //Bubble Sort
 	  for(int i = 0; i < values.length; i++) {
-		  for(int j = 0; j < values.length; j++) {
+		  for(int j = i; j < values.length; j++) {
 			  if(values[i] > values[j]) {
 				  int tmp = values[i];
 				  values[i] = values[j];
